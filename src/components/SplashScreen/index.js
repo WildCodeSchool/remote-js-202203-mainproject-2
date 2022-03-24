989494d3c136c7a63f5d26369269639805da0ad5
Kamui Shirou <x_kamui@msn.com>
2022-03-24 15:23:21 +0100

    Manage random background to show off when the application loads

--- src/components/SplashScreen/index.js
+++ src/components/SplashScreen/index.js
@@ -1,8 +1,25 @@
 import '../../assets/styles/splash.css';
 
 function SplashScreen(){
+
+    // Manage random background to show off when the application loads
+    const backgrounds = [
+        {url: 'avengers.jpg'}, 
+        {url: 'captainthor.jpg'}, 
+        {url: 'darkknight.jpg'}, 
+        {url: 'delorean.jpg'}, 
+        {url: 'halloween.jpg'}, 
+        {url: 'interstellar.jpg'}, 
+        {url: 'ironman.jpg'}, 
+        {url: 'it.jpg'}, 
+        {url: 'joker.jpg'}, 
+        {url: 'junglecruise.jpg'}, 
+        {url: 'sweeneytodd.jpg'}
+    ];
+    const randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+
     return (
-      <section id="login">
+      <section id="login" style={{backgroundImage: `url(./assets/images/backgrounds/${randomBackground.url})`}}>
 
         <h1>MyMovies</h1>
 
