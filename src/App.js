bd9cd37d1c434b0712891b2cacefd547a9c4ef16
Flichyn <nicolasflichy@gmail.com>
2022-03-25 19:29:06 +0100

    Add movie details page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/app.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import SplashScreen from './components/SplashScreen';
+import Movie from './components/Movie';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Test
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<SplashScreen />}/>
+          <Route path='/home' element={<HomePage />}/>
+          <Route path='/movie/:id' element={<Movie />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
