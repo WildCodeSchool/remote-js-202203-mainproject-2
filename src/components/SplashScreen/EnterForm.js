493097c2cf3ce5eb63fc5e38628f5b6683cf2323
Kamui Shirou <x_kamui@msn.com>
2022-03-29 15:25:33 +0200

    Change logic for main form components + Add title to the app

---
+++ src/components/SplashScreen/EnterForm.js
@@ -0,0 +1,14 @@
+function EnterForm(){
+
+    return (
+      <>
+        <h2>Welcome</h2>
+        <button id="enterApp">ENTER</button>
+        <button id="signupHere">
+          Your first time here? <span>Sign Up</span>
+        </button>
+      </>
+    );
+}
+
+export default EnterForm;
--- src/components/SplashScreen/index.js
+++ src/components/SplashScreen/index.js
@@ -1,36 +1,49 @@
+import React, { useState } from 'react';
 import '../../assets/styles/splash.css';
+import EnterForm from './EnterForm';
 
-function SplashScreen(){
+function SplashScreen() {
 
-    // Manage random background to show off when the application loads
-    const backgrounds = [
-        {url: 'avengers.jpg'}, 
-        {url: 'captainthor.jpg'}, 
-        {url: 'darkknight.jpg'}, 
-        {url: 'delorean.jpg'}, 
-        {url: 'halloween.jpg'}, 
-        {url: 'interstellar.jpg'}, 
-        {url: 'ironman.jpg'}, 
-        {url: 'it.jpg'}, 
-        {url: 'joker.jpg'}, 
-        {url: 'junglecruise.jpg'}, 
-        {url: 'sweeneytodd.jpg'}
-    ];
-    const randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+    const [randomBackground, setRandomBackground] = useState('');
+    React.useEffect(() => {
+        chooseRandomBackground();
+    }, '');
 
-    return (
-      <section id="login" style={{backgroundImage: `url(./assets/images/backgrounds/${randomBackground.url})`}}>
+    function chooseRandomBackground() {
+        setRandomBackground(
+            backgrounds[Math.floor(Math.random() * backgrounds.length)]
+        );
+    }
 
-        <h1>MyMovies</h1>
+  // Manage random background to show off when the application loads
+  const backgrounds = [
+    { url: 'avengers.jpg' },
+    { url: 'captainthor.jpg' },
+    { url: 'darkknight.jpg' },
+    { url: 'delorean.jpg' },
+    { url: 'halloween.jpg' },
+    { url: 'interstellar.jpg' },
+    { url: 'ironman.jpg' },
+    { url: 'it.jpg' },
+    { url: 'joker.jpg' },
+    { url: 'junglecruise.jpg' },
+    { url: 'sweeneytodd.jpg' },
+  ];
 
-        <div className="content">
-            <h2>Welcome</h2>
-            <button id="enterApp">ENTER</button>
-            <button id="signupHere">Your first time here? <span>Sign Up</span></button>
-        </div>
+  return (
+    <section
+      id="login"
+      style={{
+        backgroundImage: `url(./assets/images/backgrounds/${randomBackground.url})`,
+      }}
+    >
+      <h1>MyMovies</h1>
 
-      </section>
-    );
+      <div className="content">
+        <EnterForm  />
+      </div>
+    </section>
+  );
 }
 
 export default SplashScreen;
