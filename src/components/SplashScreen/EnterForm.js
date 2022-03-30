6b2af373add8fcf7e20be7bb7af3fc83b38d2836
Kamui Shirou <x_kamui@msn.com>
2022-03-30 15:30:47 +0200

    Ehancing user experience on connect!

--- src/components/SplashScreen/EnterForm.js
+++ src/components/SplashScreen/EnterForm.js
@@ -1,12 +1,22 @@
 import { Link } from 'react-router-dom';
 
-function EnterForm(){
+export default function EnterForm(){
+
+    const opening = new Audio('./assets/sounds/opening.mp3');
+    const start = () => {
+      opening.addEventListener('ended', () => {
+        console.log('prout');
+        document.getElementById('enterApp').click();
+      });
+      opening.play();
+    };
 
     return (
       <>
         <h2>Welcome</h2>
         <div className="connect-forms">
           <Link to="/login"><button id="enterApp">ENTER</button></Link>
+          <button id="enterFake" onClick={start}>ENTER</button>
           <Link to="/signup">
             <button id="signupHere" className='text-button'>Your first time here? <span>Sign Up</span></button>
           </Link>
@@ -14,5 +24,3 @@ function EnterForm(){
       </>
     );
 }
-
-export default EnterForm;
