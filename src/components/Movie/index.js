2e0502a1b67afa8f5ebf86499b198ebc5db9e5ba
quentinMlvl <quentin.malavielle@gmail.com>
2022-03-30 10:29:01 +0200

    Replace oneFullMovie.json with a js file because of Github Lint

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -4,17 +4,17 @@ import GenreList from './GenreList';
 import ActorList from './ActorList';
 import MightLike from './MightLike';
 import Rating from './Rating';
+import React from 'react';
 // import { useParams } from 'react-router-dom';
 
-/* Dummy data of a movie from the API : */
+
+/* Dummy data of a movie from the API : doesn't work with github linter ?!? */
 const movie = require('./../../oneFullMovie.json');
-// let movie = null;
-// fetch('./../../oneFullMovie.json')
-// .then(res => res.json())
-// .then(data => movie = data);
+// import movie from './oneFullMovie';
 
 
 function Movie() {
+
     //! 👇 For production only 
     // const params = useParams();
     // const movieId = params.id;
