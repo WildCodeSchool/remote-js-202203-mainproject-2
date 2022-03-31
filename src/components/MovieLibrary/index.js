8b671c33db749b7c609ef0d4f5446a38570c2762
Flichyn <nicolasflichy@gmail.com>
2022-03-31 11:38:33 +0200

    Add lazy loading on scroll

--- src/components/MovieLibrary/index.js
+++ src/components/MovieLibrary/index.js
@@ -14,9 +14,93 @@ function MovieLibrary() {
         {id: 'tt1877830', title: 'The Batman', image: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'},
         {id: 'tt1160419', title: 'Dune', image: 'https://imdb-api.com/images/original/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg'},
         {id: 'tt4016934', title: 'The Handmaiden', image: 'https://imdb-api.com/images/original/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.7273_AL_.jpg'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: 'tt0361748', title: 'Inglourious Basterds', image: 'https://imdb-api.com/images/original/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt0133093', title: 'The Matrix', image: 'https://imdb-api.com/images/original/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1028528', title: 'Death Proof', image: 'https://imdb-api.com/images/original/MV5BYTdmZmY3Y2QtNjU5NC00OGUxLTg3MWQtMmE2ODM5Mzg3MzcyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt7784604', title: 'Hereditary', image: 'https://imdb-api.com/images/original/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt5109784', title: 'Mother!', image: 'https://imdb-api.com/images/original/MV5BMzc5ODExODE0MV5BMl5BanBnXkFtZTgwNDkzNDUxMzI@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1877830', title: 'The Batman', image: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1160419', title: 'Dune', image: 'https://imdb-api.com/images/original/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt4016934', title: 'The Handmaiden', image: 'https://imdb-api.com/images/original/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.7273_AL_.jpg'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: 'tt0361748', title: 'Inglourious Basterds', image: 'https://imdb-api.com/images/original/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt0133093', title: 'The Matrix', image: 'https://imdb-api.com/images/original/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1028528', title: 'Death Proof', image: 'https://imdb-api.com/images/original/MV5BYTdmZmY3Y2QtNjU5NC00OGUxLTg3MWQtMmE2ODM5Mzg3MzcyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt7784604', title: 'Hereditary', image: 'https://imdb-api.com/images/original/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt5109784', title: 'Mother!', image: 'https://imdb-api.com/images/original/MV5BMzc5ODExODE0MV5BMl5BanBnXkFtZTgwNDkzNDUxMzI@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1877830', title: 'The Batman', image: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1160419', title: 'Dune', image: 'https://imdb-api.com/images/original/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt4016934', title: 'The Handmaiden', image: 'https://imdb-api.com/images/original/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.7273_AL_.jpg'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: 'tt0361748', title: 'Inglourious Basterds', image: 'https://imdb-api.com/images/original/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt0133093', title: 'The Matrix', image: 'https://imdb-api.com/images/original/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1028528', title: 'Death Proof', image: 'https://imdb-api.com/images/original/MV5BYTdmZmY3Y2QtNjU5NC00OGUxLTg3MWQtMmE2ODM5Mzg3MzcyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt7784604', title: 'Hereditary', image: 'https://imdb-api.com/images/original/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt5109784', title: 'Mother!', image: 'https://imdb-api.com/images/original/MV5BMzc5ODExODE0MV5BMl5BanBnXkFtZTgwNDkzNDUxMzI@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1877830', title: 'The Batman', image: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1160419', title: 'Dune', image: 'https://imdb-api.com/images/original/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt4016934', title: 'The Handmaiden', image: 'https://imdb-api.com/images/original/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.7273_AL_.jpg'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: 'tt0361748', title: 'Inglourious Basterds', image: 'https://imdb-api.com/images/original/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt0133093', title: 'The Matrix', image: 'https://imdb-api.com/images/original/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1028528', title: 'Death Proof', image: 'https://imdb-api.com/images/original/MV5BYTdmZmY3Y2QtNjU5NC00OGUxLTg3MWQtMmE2ODM5Mzg3MzcyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt7784604', title: 'Hereditary', image: 'https://imdb-api.com/images/original/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt5109784', title: 'Mother!', image: 'https://imdb-api.com/images/original/MV5BMzc5ODExODE0MV5BMl5BanBnXkFtZTgwNDkzNDUxMzI@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1877830', title: 'The Batman', image: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1160419', title: 'Dune', image: 'https://imdb-api.com/images/original/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt4016934', title: 'The Handmaiden', image: 'https://imdb-api.com/images/original/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.7273_AL_.jpg'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: 'tt0361748', title: 'Inglourious Basterds', image: 'https://imdb-api.com/images/original/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt0133093', title: 'The Matrix', image: 'https://imdb-api.com/images/original/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1028528', title: 'Death Proof', image: 'https://imdb-api.com/images/original/MV5BYTdmZmY3Y2QtNjU5NC00OGUxLTg3MWQtMmE2ODM5Mzg3MzcyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt7784604', title: 'Hereditary', image: 'https://imdb-api.com/images/original/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt5109784', title: 'Mother!', image: 'https://imdb-api.com/images/original/MV5BMzc5ODExODE0MV5BMl5BanBnXkFtZTgwNDkzNDUxMzI@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1877830', title: 'The Batman', image: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1160419', title: 'Dune', image: 'https://imdb-api.com/images/original/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt4016934', title: 'The Handmaiden', image: 'https://imdb-api.com/images/original/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.7273_AL_.jpg'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: 'tt0361748', title: 'Inglourious Basterds', image: 'https://imdb-api.com/images/original/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt0133093', title: 'The Matrix', image: 'https://imdb-api.com/images/original/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1028528', title: 'Death Proof', image: 'https://imdb-api.com/images/original/MV5BYTdmZmY3Y2QtNjU5NC00OGUxLTg3MWQtMmE2ODM5Mzg3MzcyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt7784604', title: 'Hereditary', image: 'https://imdb-api.com/images/original/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt5109784', title: 'Mother!', image: 'https://imdb-api.com/images/original/MV5BMzc5ODExODE0MV5BMl5BanBnXkFtZTgwNDkzNDUxMzI@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1877830', title: 'The Batman', image: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1160419', title: 'Dune', image: 'https://imdb-api.com/images/original/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt4016934', title: 'The Handmaiden', image: 'https://imdb-api.com/images/original/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.7273_AL_.jpg'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: 'tt0361748', title: 'Inglourious Basterds', image: 'https://imdb-api.com/images/original/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt0133093', title: 'The Matrix', image: 'https://imdb-api.com/images/original/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1028528', title: 'Death Proof', image: 'https://imdb-api.com/images/original/MV5BYTdmZmY3Y2QtNjU5NC00OGUxLTg3MWQtMmE2ODM5Mzg3MzcyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt7784604', title: 'Hereditary', image: 'https://imdb-api.com/images/original/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt5109784', title: 'Mother!', image: 'https://imdb-api.com/images/original/MV5BMzc5ODExODE0MV5BMl5BanBnXkFtZTgwNDkzNDUxMzI@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1877830', title: 'The Batman', image: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1160419', title: 'Dune', image: 'https://imdb-api.com/images/original/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt4016934', title: 'The Handmaiden', image: 'https://imdb-api.com/images/original/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.7273_AL_.jpg'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: 'tt0361748', title: 'Inglourious Basterds', image: 'https://imdb-api.com/images/original/MV5BOTJiNDEzOWYtMTVjOC00ZjlmLWE0NGMtZmE1OWVmZDQ2OWJhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt0133093', title: 'The Matrix', image: 'https://imdb-api.com/images/original/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1028528', title: 'Death Proof', image: 'https://imdb-api.com/images/original/MV5BYTdmZmY3Y2QtNjU5NC00OGUxLTg3MWQtMmE2ODM5Mzg3MzcyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt7784604', title: 'Hereditary', image: 'https://imdb-api.com/images/original/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt5109784', title: 'Mother!', image: 'https://imdb-api.com/images/original/MV5BMzc5ODExODE0MV5BMl5BanBnXkFtZTgwNDkzNDUxMzI@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1877830', title: 'The Batman', image: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt1160419', title: 'Dune', image: 'https://imdb-api.com/images/original/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg'},
+        {id: 'tt4016934', title: 'The Handmaiden', image: 'https://imdb-api.com/images/original/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.7273_AL_.jpg'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
+        {id: '4564564ff', title: 'BLABLABLA', image: 'BLABLABLA'},
     ];
 
     const [searchResults, setSearchResults] = React.useState('');
+    const [library, setLibrary] = React.useState(entireLibrary);
+    const [toLoad, setToLoad] = React.useState(30);
 
     function handleSearch(event) {
         const input = event.target;
@@ -24,13 +108,31 @@ function MovieLibrary() {
         input.value === '' && setSearchResults(input.value);
     }
 
+    function loadMore(event) {
+        if (entireLibrary.length > toLoad) {
+            if (event.target.scrollTop >= event.target.scrollHeight - 1100) {
+                setToLoad(toLoad + 30);
+                setLibrary(entireLibrary.slice(0, toLoad));
+                console.log(toLoad);
+            }
+        }
+    }
+
+    window.onbeforeunload = () => {
+        document.querySelector('.movie-library').scrollTo(0, 0);
+    };
+
+    React.useEffect(() => {
+        setLibrary(entireLibrary.slice(0, toLoad));
+    }, [toLoad]);
+
     return (
         <section id="mainApp">
-            <div className="movie-library">
+            <div className="movie-library" onScroll={() => loadMore(event)}>
                 <div className="wrapper-library">
                     <div className="scrollable">
                         <div className="wrapper">
-                            <h1>My movie library</h1>
+                            <h1>My Movie Library</h1>
                             <LibrarySearch handleSearch={handleSearch}/>
                             <div className='library'>
                                 {library.filter((movie) => movie.title.toLowerCase()
